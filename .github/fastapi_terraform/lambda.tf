842b308f8673c97957f91056374f3c62c7172d79
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-21 08:45:34 +0900

    dev

--- .github/fastapi_terraform/lambda.tf
+++ .github/fastapi_terraform/lambda.tf
@@ -21,7 +21,7 @@ resource "aws_iam_role" "lambda_exec_role" {
 
 resource "aws_iam_role_policy_attachment" "logs_full_access" {
   role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb_full_access" {
