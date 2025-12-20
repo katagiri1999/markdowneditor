272af596661652d60467a9b6eaa467a739b11b5e
yuutakatagiri <yuutakatagiri9@gmail.com>
2025-12-21 06:13:00 +0900

    dev

--- react/src/components/explorer.tsx
+++ react/src/components/explorer.tsx
@@ -4,7 +4,7 @@ import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import Box from "@mui/material/Box";
 import Typography from '@mui/material/Typography';
 import { RichTreeView } from '@mui/x-tree-view/RichTreeView';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import userStore from "../store/user_store";
@@ -15,39 +15,17 @@ import TreeUpdate from './tree_update';
 function Explorer() {
   const navigate = useNavigate();
   const { id_token, tree } = userStore();
-
-  const url_node_id = utils.get_url_node_id();
-
-  const [currentNodeId, setCurrentNodeId] = useState(url_node_id);
   const [expandedItems, setExpandedItems] = useState(["/Folder"]);
-  const [hasInitialized, setHasInitialized] = useState(false);
 
-  useEffect(() => {
-    if (!url_node_id) return;
-    const timer = setTimeout(() => {
-      setCurrentNodeId(url_node_id || "");
-    }, 0);
-    return () => clearTimeout(timer);
-  }, [url_node_id]);
-
-  useEffect(() => {
-    if (!hasInitialized && tree && url_node_id) {
-      const parents = utils.get_parent_node_ids(url_node_id);
-      const latest_expanded_items = parents ? [...new Set([...expandedItems, ...parents])] : null;
-      const timer = setTimeout(() => {
-        if (latest_expanded_items) setExpandedItems(latest_expanded_items);
-        setHasInitialized(true);
-      }, 0);
-      return () => clearTimeout(timer);
-    }
-  }, [tree, url_node_id, hasInitialized, expandedItems]);
+  const url_node_id = utils.get_url_node_id() || "/Folder";
+  const parents = utils.get_parent_node_ids(url_node_id);
+  const displayedExpanded = [...new Set([...expandedItems, ...parents])];
 
   const handleItemClick = (_: React.MouseEvent<Element, MouseEvent>, itemId: string) => {
-    setCurrentNodeId(itemId);
     navigate(`/main?node_id=${itemId}`);
   };
 
-  if (id_token && tree && currentNodeId) {
+  if (id_token && tree) {
     return (
       <>
         <Typography variant="h5" sx={{ my: 2 }}>
@@ -58,8 +36,8 @@ function Explorer() {
           <RichTreeView
             items={[tree]}
             onItemClick={handleItemClick}
-            selectedItems={currentNodeId}
-            expandedItems={expandedItems}
+            selectedItems={url_node_id}
+            expandedItems={displayedExpanded}
             onExpandedItemsChange={(_, ids) => setExpandedItems(ids)}
             slots={{
               expandIcon: FolderIcon,
@@ -67,7 +45,7 @@ function Explorer() {
               endIcon: ArticleOutlinedIcon,
             }}
           />
-          <TreeUpdate currentNodeId={currentNodeId} />
+          <TreeUpdate currentNodeId={url_node_id} />
         </Box>
       </>
     );
