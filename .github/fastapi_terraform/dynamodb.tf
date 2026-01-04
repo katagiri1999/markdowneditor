2e0a3869be56e7a095c43533438114e45cf510c4
yuutakatagiri <yuutakatagiri9@gmail.com>
2026-01-04 14:23:22 +0900

    update terraform for #47

--- .github/fastapi_terraform/dynamodb.tf
+++ .github/fastapi_terraform/dynamodb.tf
@@ -1,54 +1,19 @@
-resource "aws_dynamodb_table" "users_table" {
-  name         = "${var.project_name}-users-table"
+resource "aws_dynamodb_table" "table" {
+  name         = "${var.project_name}-table"
   billing_mode = "PROVISIONED"
 
   read_capacity  = 1
   write_capacity = 1
 
-  hash_key = "email"
+  hash_key = "PK"
   attribute {
-    name = "email"
+    name = "PK"
     type = "S"
   }
 
-  tags = {
-    Name = "${var.project_name}"
-  }
-}
-
-resource "aws_dynamodb_table" "trees_table" {
-  name         = "${var.project_name}-trees-table"
-  billing_mode = "PROVISIONED"
-
-  read_capacity  = 1
-  write_capacity = 1
-
-  hash_key = "email"
+  range_key = "SK"
   attribute {
-    name = "email"
-    type = "S"
-  }
-
-  tags = {
-    Name = "${var.project_name}"
-  }
-}
-
-resource "aws_dynamodb_table" "nodes_table" {
-  name         = "${var.project_name}-nodes-table"
-  billing_mode = "PROVISIONED"
-
-  read_capacity  = 1
-  write_capacity = 1
-
-  hash_key = "email"
-  attribute {
-    name = "email"
-    type = "S"
-  }
-  range_key = "id"
-  attribute {
-    name = "id"
+    name = "SK"
     type = "S"
   }
 
