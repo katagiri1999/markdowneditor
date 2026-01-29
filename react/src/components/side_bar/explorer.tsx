6ccfd6e3644f984cd2d50fd0980bcc2f693786ba
yuutakatagiri <yuutakatagiri9@gmail.com>
2026-01-29 21:18:41 +0900

    some

--- react/src/components/side_bar/explorer.tsx
+++ react/src/components/side_bar/explorer.tsx
@@ -5,36 +5,18 @@ import { useNavigate } from "react-router-dom";
 
 import type { Tree } from '@/src/lib/types';
 
-import RequestHandler from "@/src/lib/request_handler";
 import TreeHandler from '@/src/lib/tree_handler';
-import loadingState from "@/src/store/loading_store";
-import userStore from "@/src/store/user_store";
 
 
 function Explorer(props: { node_id: string, tree: Tree }) {
   const navigate = useNavigate();
-  const { id_token, setTree } = userStore();
-  const { setLoading } = loadingState();
   const [expandedItems, setExpandedItems] = useState<string[]>([]);
 
-  const requests = new RequestHandler(id_token);
   const tree_handler = new TreeHandler(props.tree);
   const parents = tree_handler.getParentNodeIds(props.node_id);
   const displayedExpanded = [
     ...new Set([props.tree.id, ...expandedItems, ...parents]),
   ];
-
-  async function updateLabel(node_id: string, label: string) {
-    setLoading(true);
-
-    const res = await requests.put<Tree>(
-      `${import.meta.env.VITE_API_HOST}/api/tree/node/label/${node_id}`,
-      { label: label }
-    );
-
-    setTree(res.body);
-    setLoading(false);
-  }
 
   return (
     <>
@@ -48,8 +30,6 @@ function Explorer(props: { node_id: string, tree: Tree }) {
           selectedItems={props.node_id}
           expandedItems={displayedExpanded}
           onExpandedItemsChange={(_, ids) => setExpandedItems(ids)}
-          isItemEditable
-          onItemLabelChange={(itemId, label) => updateLabel(itemId, label)}
         />
       </Box>
     </>
